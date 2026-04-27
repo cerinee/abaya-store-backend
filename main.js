c46b4c30b2b5d25fffd3ee075933a031a3a0bf73
cerine.c <bendjennicerine@gmail.com>
2026-04-27 14:38:37 +0100

    fixed frontend with backend

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const allRoutes = require('./routes/AllRoutes');
 const multer = require('./middleware/multer');
 app.use(express.json());
 app.use(cors({
-    origin: 'https://lyrine-store.netlify.app/'
+    origin: ['http://localhost:5173', 'https://lyrine-store.netlify.app/']
 }));
 app.use("/uploads", express.static("uploads"));
 
